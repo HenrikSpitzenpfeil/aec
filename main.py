c693b4267e66e98e26926251556048304390c7ba
Henrik Spitzenpfeil <henrik.spitzenpfeil@gmail.com>
2023-05-24 13:58:31 +0200

    added further logic

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from autolab import autolab
 def main():
     #microscope = secm.SECM(self, config, config, Electrode, substrate)
     #experiment = Experiments.DummyExperiment(self, microscope)
-    #number_of_experiments = int(input('Please insert Number of Experiments to perform'))
-    #experiments_performed = 0
+    number_of_experiments = int(input('Please insert Number of Experiments to perform'))
+    experiments_performed = 0
     
-    #While True:
-    # if experiments_performed == number_of_experiments:
-    #  break
-    # if spots_left = 0
-    #   secm.new_substrate
-    # experiment.measure
-    # secm.next_spot
-    # experiments_performed += 1
+    while True:
+        if experiments_performed == number_of_experiments:
+          break
+        # elif secm.spots_left == 0:
+        #   secm.new_substrate()
+        # else:
+            # experiment.measure
+            # secm.next_spot
+            #experiments_performed += 1
