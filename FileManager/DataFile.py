684d94abcc64ffdb9c3b98e6443a5460af98bf5d
Henrik Spitzenpfeil <henrik.spitzenpfeil@gmail.com>
2023-07-13 13:14:48 +0200

    Added comments and documentation

--- FileManager/DataFile.py
+++ FileManager/DataFile.py
@@ -8,6 +8,9 @@ import pandas as pd
 @dataclass
 class SECMDataFile:
 
+    """ Dataclass for an SECM experiment
+    containing the dataset aswell as the metadata for it."""
+
     file_path: os.PathLike
     data: pd.DataFrame
     experiment_name: str
@@ -21,12 +24,15 @@ class SECMDataFile:
     
 
     def write_to_csv(self) -> None:
-        if os.path.isfile(self.file_path):
+
+        """ Writes the data to a CSV file at the specified file path."""
+
+        if os.path.isfile(self.file_path): #Check if the file already exists so not to overwrite existing data
             print ("File already exists")
             return
         
         with open(self.file_path, 'w',newline='') as csv_file:
-            writer = csv.writer(csv_file)
+            writer = csv.writer(csv_file) # Write the CSV file header
 
             writer.writerow(("Experiment Name", self.experiment_name))
             writer.writerow(("Experiment Id", self.experiment_id))
@@ -41,20 +47,21 @@ class SECMDataFile:
         self.data.to_csv(self.file_path,
                          sep = ",",
                          mode = "a",
-                         header = True)
+                         header = True) #appends the data to the CSV file.
 
 def parse_secm_datafile(file_path) -> SECMDataFile:
 
     """Reads the SECM Data into a SECMDataFile object."""
+
     with open(file_path, 'r', newline= '') as csv_file:
         reader = csv.reader(csv_file)
 
         attributes = []
-        for line in reader:
+        for line in reader: #collects alls attributes from the csv file in a list
             if line == []:
                 break
             attributes.append(line[1])
     #TODO: Remove the magic number somehow
-    dataframe = pd.read_csv(file_path, sep = ',', skiprows= 9)
+    dataframe = pd.read_csv(file_path, sep = ',', skiprows= 9) # get the data from the file to a dataframe
     
     return SECMDataFile(file_path, dataframe, *attributes)
